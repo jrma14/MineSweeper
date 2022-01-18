c4b1eb5e1d013d0a659349be3f0059b885fd4e9b
Jacob <jrmadams02@gmail.com>
2022-01-18 16:41:16 -0500

    formatting

--- minesweeper.py
+++ minesweeper.py
@@ -20,10 +20,9 @@ difficultySettings = {
 
 difficulty = 'custom'
 
-
 width = 1920
 height = 1080
-gridSize = (width/cols, height/rows)
+gridSize = (width / cols, height / rows)
 
 buttonWidth = 300
 buttonHeight = height / 6
@@ -75,8 +74,6 @@ def displayBoard(board):
             scaledSurface = pygame.transform.scale(surface, (gridSize[0], gridSize[1]))
             screen.blit(scaledSurface, (gridSize[0] * j, gridSize[1] * i))
 
-def checkButtons():
-    pass
 
 def displayMenu():
     color = (0, 0, 0)
@@ -105,6 +102,7 @@ def displayMenu():
     screen.blit(settingsText, settingsTextRect)
     screen.blit(exitText, exitTextRect)
 
+
 def displaySettings():
     color = (0, 0, 0)
     screen.fill(backgroundColor)
@@ -132,6 +130,7 @@ def displaySettings():
     screen.blit(settingsText, settingsTextRect)
     screen.blit(exitText, exitTextRect)
 
+
 def displayPause():
     color = (0, 0, 0)
     play = pygame.Rect(center[0] - buttonWidth / 2, center[1] - buttonHeight / 2 - (buttonHeight + distBetweenButtons),
@@ -158,6 +157,7 @@ def displayPause():
     screen.blit(settingsText, settingsTextRect)
     screen.blit(exitText, exitTextRect)
 
+
 def displayEndgame(win):
     gameOverText = font.render('Game Over', True, (255, 0, 0), (255, 255, 255))
     gameOverTextRect = gameOverText.get_rect()
@@ -171,6 +171,7 @@ def displayEndgame(win):
     screen.blit(gameOverText, gameOverTextRect)
     screen.blit(winLossText, winLossTextRect)
     screen.blit(exitText, exitTextRect)
+
 
 def displayBombs(board):
     surface = pygame.Surface((128, 128))
@@ -196,33 +197,35 @@ while not exit:
         if event.type == pygame.MOUSEBUTTONDOWN and settings:
             pos = pygame.mouse.get_pos()
             if event.button == 1:
-                checkButtons()
                 if center[0] - buttonWidth / 2 <= pos[0] <= center[0] + buttonWidth / 2:
                     if center[1] - buttonHeight / 2 - (buttonHeight + distBetweenButtons) <= pos[1] \
                             <= center[1] - buttonHeight / 2 - distBetweenButtons:  # easy
                         difficulty = 'easy'
-                        gridSize = (width / difficultySettings[difficulty][0], height / difficultySettings[difficulty][1])
+                        gridSize = (
+                        width / difficultySettings[difficulty][0], height / difficultySettings[difficulty][1])
                         settings = False
                         break
                     if center[1] - buttonHeight / 2 <= pos[1] <= center[1] + buttonHeight / 2:  # medium
                         difficulty = 'medium'
-                        gridSize = (width / difficultySettings[difficulty][0], height / difficultySettings[difficulty][1])
+                        gridSize = (
+                        width / difficultySettings[difficulty][0], height / difficultySettings[difficulty][1])
                         settings = False
                         break
                     if center[1] + buttonHeight / 2 + distBetweenButtons <= pos[1] \
                             <= center[1] + 3 * buttonHeight / 2 + distBetweenButtons:  # hard
                         difficulty = 'hard'
-                        gridSize = (width / difficultySettings[difficulty][0], height / difficultySettings[difficulty][1])
+                        gridSize = (
+                        width / difficultySettings[difficulty][0], height / difficultySettings[difficulty][1])
                         settings = False
                         break
         if event.type == pygame.MOUSEBUTTONDOWN and not settings:
             pos = pygame.mouse.get_pos()
             if event.button == 1:
-                checkButtons()
                 if center[0] - buttonWidth / 2 <= pos[0] <= center[0] + buttonWidth / 2:
                     if center[1] - buttonHeight / 2 - (buttonHeight + distBetweenButtons) <= pos[1] \
                             <= center[1] - buttonHeight / 2 - distBetweenButtons and not settings:  # play
-                        board.randomizeBoard(difficultySettings[difficulty][0], difficultySettings[difficulty][1], difficultySettings[difficulty][2])
+                        board.randomizeBoard(difficultySettings[difficulty][0], difficultySettings[difficulty][1],
+                                             difficultySettings[difficulty][2])
                         running = True
                     if center[1] - buttonHeight / 2 <= pos[1] <= center[1] + buttonHeight / 2:  # settings
                         print('Settings!')
@@ -285,13 +288,13 @@ while not exit:
             if event.type == pygame.MOUSEBUTTONDOWN and pause:
                 pos = pygame.mouse.get_pos()
                 if event.button == 1:
-                    checkButtons()
                     if center[0] - buttonWidth / 2 <= pos[0] <= center[0] + buttonWidth / 2:
                         if center[1] - buttonHeight / 2 - (buttonHeight + distBetweenButtons) <= pos[1] \
                                 <= center[1] - buttonHeight / 2 - distBetweenButtons:  # resume
                             pause = False
                         if center[1] - buttonHeight / 2 <= pos[1] <= center[1] + buttonHeight / 2:  # restart
-                            board.randomizeBoard(difficultySettings[difficulty][0], difficultySettings[difficulty][1], difficultySettings[difficulty][2])
+                            board.randomizeBoard(difficultySettings[difficulty][0], difficultySettings[difficulty][1],
+                                                 difficultySettings[difficulty][2])
                             pause = False
                         if center[1] + buttonHeight / 2 + distBetweenButtons <= pos[1] \
                                 <= center[1] + 3 * buttonHeight / 2 + distBetweenButtons:  # back to menu
@@ -300,7 +303,6 @@ while not exit:
             if event.type == pygame.MOUSEBUTTONDOWN and endgame:
                 pos = pygame.mouse.get_pos()
                 if event.button == 1:
-                    checkButtons()
                     if center[0] - buttonWidth / 2 <= pos[0] <= center[0] + buttonWidth / 2:
                         if center[1] + buttonHeight / 2 + distBetweenButtons <= pos[1] \
                                 <= center[1] + 3 * buttonHeight / 2 + distBetweenButtons:  # back to menu
